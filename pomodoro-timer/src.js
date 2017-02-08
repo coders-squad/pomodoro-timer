7fb27ae5ace23fc340b01b078d6bae23c36b0c0c
shahenaz Monia <shahy.m.93@gmail.com>
2017-02-08 23:07:25 +0200

    connect methods with html design

--- pomodoro-timer/src.js
+++ pomodoro-timer/src.js
@@ -8,43 +8,57 @@ var a_time = {
 
 var  start;
 var run = true;
+var time;
 
 var programInterface ={
 
 timing : function() {
-  a_time.seconds = 60;
-if (run){
-console.log("Timeing");
-start = setInterval( function(){
-if(a_time.seconds > 0 && a_time.minutes > 0 ){
-  console.log("Timeing seconds");
-a_time.seconds--;
-}else if (a_time.seconds == 0 && a_time.minutes > 0) {
-a_time--;
-a_time.seconds=60;
-}else if (a_time.seconds == 0 && a_time.minutes == 0) {
-pause();
-}
-},1000);
+      a_time.seconds = 60;
+    if (run){
+  //  console.log("Timeing");
+    start = setInterval( function(){
+    if(a_time.seconds > 0 && a_time.minutes > 0 ){
+    //  console.log("Timeing seconds");
+      a_time.seconds--;
+    }else if (a_time.seconds == 0 && a_time.minutes > 0) {
+      a_time.minutes--;
+      a_time.seconds=59;
+    }else if (a_time.seconds == 0 && a_time.minutes == 0) {
+       clearInterval(programInterface.timing());
+      //pause();
+    }
 
-}},
+    time = a_time.minutes + " : " + a_time.seconds;
+    document.getElementById('time').innerHTML = time;
+    },1000);
+
+
+    }},
 
 gettime: function(){
-console.log(a_time);
-return a_time;
+  console.log(a_time);
+  return a_time;
 },
 
 pause : function(){
-run = false;
-clearInterval(start);
+  run = false;
+  time = a_time.minutes + " : " + a_time.seconds;
+  clearInterval(start);
+
 },
 
 reset : function() {
 
-a_time.minutes= initialInput;
-a_time.seconds= 0;
-pause();
-
+  // a_time.minutes= initialInput;
+  // a_time.seconds= 0;
+  // //pause();
+  run = false;
+       clearInterval(start);
+       a_time.minutes = initialInput;
+       a_time.seconds = 0;
+       time = a_time.minutes + " : " + a_time.seconds;
+       pause();
+       document.getElementById('time').innerHTML = time;
 }
 
 
