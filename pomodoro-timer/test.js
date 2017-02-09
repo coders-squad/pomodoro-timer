45497d871df61f64b020b4499b15bb16cb0dd2a9
Akram Badah <Akram.Badah@gmail.com>
2017-02-09 10:10:28 +0200

    Update test.js

--- pomodoro-timer/test.js
+++ pomodoro-timer/test.js
@@ -3,68 +3,72 @@ var test = QUnit.test;
 test("check if the function exists",function(t){
   t.ok(window.program,"it exists");
 });
-
+document.getElementById('start').onclick = function(){
 test("Timer should start at 25:00 minutes ",function(t){
   var time = program(25);
   t.deepEqual(time.gettime(), {minutes:25,seconds:0} , "Timer starts at 25:00");
 
 });
 
-test("Start Timing() ", function(t) {
-      var Timer = program(25);
-      Timer.timing();
+	test("Start Timing() ", function(t) {
+		
+	 	  var Timer = program(25);
+		  
+		  Timer.timing();
+		  var done = t.async();
+		 // start interval
+		  setTimeout(function() {
+		    t.equal( Timer.gettime().seconds,59,"It decrease time");
 
-      var done = t.async();
-     // start interval
-      setTimeout(function() {
-        t.equal( Timer.gettime().seconds,59,"It decrease time");
+		    ok( true, "timing done" );
+		    done();
+		  }, 1000);
+	});
 
-        ok( true, "timing done" );
-        done();
-      }, 1000);
-  });
+}
+document.getElementById('stop').onclick = function(){
+	test("Method pause should stop timing",function(t){
+	  var Timer = program(25);
+	  Timer.timing();
+	  var done1 = t.async();
+	  var done  = t.async();
+	  setTimeout(function() {
+	    Timer.pause();
+	    ok( true, "timing paused" );
+	    done1();
+	  }, 1000);
 
+	  setTimeout(function() {
 
-test("Method pause should stop timing",function(t){
-  var Timer = program(25);
-  Timer.timing();
-  var done1 = t.async();
-  var done  = t.async();
-  setTimeout(function() {
-    Timer.pause();
-    ok( true, "timing paused" );
-    done1();
-  }, 1000);
+	    t.equal( Timer.gettime().seconds,59,"It decrease time");
 
-  setTimeout(function() {
+	    ok( true, "timing paused" );
+	    done();
+	  }, 3000);
 
-    t.equal( Timer.gettime().seconds,59,"It decrease time");
+	});
+}
+document.getElementById('reset').onclick = function(){
+	test("Method reset() should set Time initial Input",function(t){
 
-    ok( true, "timing paused" );
-    done();
-  }, 3000);
+	  var Timer = program(25);
+	  //Timer.timing();
+	 var done1 = t.async();
+	  var done  = t.async();
+	  setTimeout(function() {
+	    Timer.reset();
+	    ok( true, "timing reset" );
+	    done1();
+	  }, 1000);
 
-});
+	  setTimeout(function() {
 
-test("Method reset() should set Time initial Input",function(t){
+	    t.deepEqual( Timer.gettime(),{minutes:25 , seconds:0},"reset time");
 
-  var Timer = program(25);
-  Timer.timing();
- var done1 = t.async();
-  var done  = t.async();
-  setTimeout(function() {
-    Timer.reset();
-    ok( true, "timing reset" );
-    done1();
-  }, 1000);
-
-  setTimeout(function() {
-
-    t.deepEqual( Timer.gettime(),{minutes:25 , seconds:0},"reset time");
-
-    ok( true, "timing reset" );
-    done();
-  }, 3000);
+	    ok( true, "timing reset" );
+	    done();
+	  }, 3000);
 
 
-});
+	});
+}
